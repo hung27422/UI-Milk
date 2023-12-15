554b74ea8222f25b236a564f8bb98f3ba7c1a256
Ho Tan Hung <tanhungho2002@gmail.com>
2023-12-15 09:55:55 +0700

    deploy

--- src/AuTh0/AuthProvider.js
+++ src/AuTh0/AuthProvider.js
@@ -7,7 +7,7 @@ function AuthProvider({ children }) {
       domain="dev-zse8qge31om0dwlh.us.auth0.com"
       clientId="6mNu2Dwxqvai9vX65n26bX59ZB82DspH"
       authorizationParams={{
-        redirect_uri: "http://localhost:3000/",
+        redirect_uri: "https://ui-milk.vercel.app/",
       }}
     >
       {children}
